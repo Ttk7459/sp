51a2d15af499719a40c712dbe940b3dba947d2eb
Ttk7459 <tolera2001@gmail.com>
2025-02-13 11:18:46 +0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import FingerprintChart from "./FingerprintChart";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Fingerprint Accuracy Over Time</h1>
+      <FingerprintChart />
+    </div>
+  );
+}
+
+export default App;
